b2fcc3bba3e4ce0921dc7606a83c70baae6ad7ca
super3 <me@super3.org>
2014-01-21 02:09:13 -0500

    Added Analytics

--- header.php
+++ header.php
@@ -36,6 +36,16 @@
     <link rel="apple-touch-icon-precomposed" href="img/social-icons/primecoin-apple-57.png">
     <link rel="shortcut icon" href="img/sample/logo.png">
 
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-40768937-6', 'primecoin.io');
+    ga('send', 'pageview');
+
+    </script>
   </head>
 
   <body>
