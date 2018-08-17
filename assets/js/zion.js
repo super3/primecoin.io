0dee06f57e6efc224ecc0d7eeea70d0a349335a9
HarryeTian <39288542+HarryeTian@users.noreply.github.com>
2018-08-17 16:21:49 +0800

    Update zion.js

--- assets/js/zion.js
+++ assets/js/zion.js
@@ -19,27 +19,4 @@ $(document).ready(function () {
             parallax();
         });
     }
-
-    //动态添加js
-    // if (x >= 975) {
-    //     $(".page-show").after("<script src='assets/js/dynamic_nav.js'></script>");
-    // } else {
-    //     console.log();
-    // }
-    //
-    // $(window).resize(function () {
-    //     if ($(window).width() >= 975) {
-    //         console.log("rxes")
-    //
-    //         $(window).scrollTop(0);
-    //         var t = $(".page-show").next()[0];
-    //         if (!t) {
-    //             // console.log("前" + $(".page-show").next()[0].nodeName);
-    //             $(".page-show").after("<script src='assets/js/dynamic_nav.js'></script>");
-    //             // console.log("后" + $(".page-show").next()[0].nodeName);
-    //         }
-    //     } else {
-    //         $(".page-show").next().remove();
-    //     }
-    // });
 });
